87dea8ef6e0377406cb00318f562f69a85d8a80d
csastry12 <csastry12@yahoo.com>
2025-10-29 12:28:29 -0500

    Update direct_prompt_agent.py

--- project/starter/phase_1/direct_prompt_agent.py
+++ project/starter/phase_1/direct_prompt_agent.py
@@ -1,6 +1,6 @@
 # Test script for DirectPromptAgent class
 
-from WorkflowAgents.# TODO: 1 - Import the DirectPromptAgent class from BaseAgents
+from workflow_agents import DirectPromptAgent # TODO: 1 - Import the DirectPromptAgent class from BaseAgents
 import os
 from dotenv import load_dotenv
 
@@ -8,16 +8,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # TODO: 2 - Load the OpenAI API key from the environment variables
-openai_api_key = 
+openai_api_key = os.getenv('OPENAI_API_KEY')
+base_url =  os.getenv('OPENAI_BASE_URL')
 
 prompt = "What is the Capital of France?"
 
 # TODO: 3 - Instantiate the DirectPromptAgent as direct_agent
+
+direct_prompt_agent = DirectPromptAgent(openai_api_key, base_url)
+
 # TODO: 4 - Use direct_agent to send the prompt defined above and store the response
-direct_agent_response = 
+direct_agent_response = direct_prompt_agent.respond(prompt)
 
 # Print the response from the agent
 print(direct_agent_response)
 
 # TODO: 5 - Print an explanatory message describing the knowledge source used by the agent to generate the response
-print()
+print("This agent used general knowledge from the LLM")
+
